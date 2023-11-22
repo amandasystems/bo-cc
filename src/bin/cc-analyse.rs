b2122d5ee32bd816a329cfb243a7e6d9ff6c1abc
Amanda Stjerna <amanda.stjerna@it.uu.se>
2023-11-22 15:33:00 +0100

    Faster summary

--- src/bin/cc-analyse.rs
+++ src/bin/cc-analyse.rs
@@ -12,36 +12,58 @@ enum Cmd {
 fn cmd_summarise(warcs: Vec<String>) {
     let nr_warcs = warcs.len();
 
-    let summary = warcs
-        .par_iter()
-        .flat_map(|warc| ArchiveSummary::from_file(&to_storage_fn(&warc)))
-        .reduce(ArchiveSummary::default, |a, b| a.merge(b));
-
-    let nr_urls_with_patterns = summary.urls_with_pattern_forms.len() as f64;
-    let successful_urls = nr_urls_with_patterns as f64 + summary.nr_urls_without_patterns as f64;
-    let total_urls = successful_urls + summary.nr_unknown_encoding as f64;
-    let nr_forms_with_patterns: usize = summary
-        .urls_with_pattern_forms
+    let (
+        urls_with_pattern,
+        total_urls,
+        forms_w_pattern,
+        total_forms,
+        nr_unknown_encoding,
+        successful_urls,
+    ) = warcs
         .iter()
-        .map(|u| u.with_patterns.len())
-        .sum();
-    let total_forms = summary.nr_forms_without_patterns as f64 + nr_forms_with_patterns as f64;
+        .flat_map(|warc| ArchiveSummary::from_file(&to_storage_fn(&warc)))
+        .fold(
+            (0, 0, 0, 0, 0, 0),
+            |(
+                urls_with_pattern,
+                total_urls,
+                forms_w_pattern,
+                total_forms,
+                unknown_encoding,
+                successful_urls,
+            ),
+             next| {
+                let next_urls_w_pattern = next.urls_with_pattern_forms.len() as i64;
+                let successful = next_urls_w_pattern + next.nr_urls_without_patterns;
+                let next_forms_w_pattern: i64 = next
+                    .urls_with_pattern_forms
+                    .iter()
+                    .map(|u| u.with_patterns.len() as i64)
+                    .sum();
+
+                (
+                    urls_with_pattern + next_urls_w_pattern,
+                    total_urls + successful + next.nr_unknown_encoding,
+                    forms_w_pattern + next_forms_w_pattern,
+                    total_forms + next_forms_w_pattern + next.nr_forms_without_patterns,
+                    unknown_encoding + next.nr_unknown_encoding,
+                    successful_urls + successful,
+                )
+            },
+        );
 
     println!("Processed {nr_warcs} WARCs with {total_urls} URLs ({successful_urls} OK). Results: ");
     println!(
-        "Nr URLs with unknown encoding, broken HTML, etc: {} ({:.4}%)",
-        summary.nr_unknown_encoding,
-        summary.nr_unknown_encoding as f64 / total_urls
+        "Nr URLs with unknown encoding, broken HTML, etc: {nr_unknown_encoding} ({:.4}%)",
+        nr_unknown_encoding as f64 / total_urls as f64
     );
     println!(
-        "URLs with pattern/s: {}, ({:.1}%)",
-        nr_urls_with_patterns,
-        100f64 * (nr_urls_with_patterns / total_urls)
+        "URLs with pattern/s: {urls_with_pattern}, ({:.1}%)",
+        100f64 * (urls_with_pattern as f64 / total_urls as f64)
     );
     println!(
-        "Forms with patterns: {} ({:.1}%)",
-        nr_forms_with_patterns,
-        100f64 * (nr_forms_with_patterns as f64 / total_forms)
+        "Forms with patterns: {forms_w_pattern} ({:.1}%)",
+        100f64 * (forms_w_pattern as f64 / total_forms as f64)
     );
 }
 
